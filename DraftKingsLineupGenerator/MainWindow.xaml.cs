d5a6785f7bb99f205f57428a787defe88e0c9cc6
samueldavidcrawford <samueldavidcrawford1@gmail.com>
2016-08-14 16:57:42 -0500

    Added browse button functionality

--- DraftKingsLineupGenerator/MainWindow.xaml.cs
+++ DraftKingsLineupGenerator/MainWindow.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+using System.Windows.Controls;
+
 
 namespace DraftKingsLineupGenerator
 {
@@ -20,9 +24,38 @@ namespace DraftKingsLineupGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string _filePath;
+
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Set filter options and filter index.
+            openFileDialog1.Filter = "CSV Files (.csv)|*.csv";
+            openFileDialog1.FilterIndex = 1;
+
+            openFileDialog1.Multiselect = false;
+            
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                _filePath = openFileDialog1.FileName;
+                try
+                {
+                    _filePath = openFileDialog1.FileName;
+                }
+                catch (Exception)
+                {
+                    throw new ArgumentNullException("Cannot load the file.");
+                }
+            }
 
         }
     }
